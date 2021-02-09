6913b5ff6d33e4bef70a1fa222e9ef16d67000a0
Alessandro Perelli <alper@dtu.dk>
2021-02-09 16:25:59 +0100

    Add files via upload

---
+++ Fig_Block_Leverage_Scores/LeverageScoreSampling.m
@@ -0,0 +1,11 @@
+function [C, idx] = LeverageScoreSampling(A, s)
+n = size(A, 2);
+[~, ~, V] = svd(A);
+leveragescores = sum(V.^2, 2);
+prob = leveragescores / sum(leveragescores);
+idx = randsample(n, s, true, prob);
+idx = unique(idx); % eliminate duplicates
+C = A(:, idx);
+end
+
+% A' = USV'
